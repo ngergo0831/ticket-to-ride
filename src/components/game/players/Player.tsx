23e24ad9883e603ed7248a36a59fb1d3db1dce51
ngergo0831 <ngergo0831@gmail.com>
2021-04-11 20:32:18 +0200

    Typescript magic

--- src/components/game/players/Player.tsx
+++ src/components/game/players/Player.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React from "react";
 
-function Player() {
+type PlayerProp = {
+    name?: string;
+    vagons?: number;
+    card1?: number;
+    card2?: number;
+};
+
+function Player({
+    name = "Player",
+    vagons = 35,
+    card1 = 10,
+    card2 = 4,
+}: PlayerProp) {
     return (
         <div className="player-title-box">
-        <div className="player-title">Name</div>
-        <div className="player-title__flex">
-            <div className="player-title__vagons">33</div>
-            <div className="player-title__card1">22</div>
-            <div className="player-title__card2">10</div>
+            <div className="player-title">{name}</div>
+            <div className="player-title__flex">
+                <div className="player-title__vagons">{vagons}</div>
+                <div className="player-title__card1">{card1}</div>
+                <div className="player-title__card2">{card2}</div>
+            </div>
         </div>
-        </div>
-    )
+    );
 }
 
-export default Player
+export default Player;
--- src/components/game/players/Players.tsx
+++ src/components/game/players/Players.tsx
@@ -1,16 +1,38 @@
-import React from "react";
+import { useState } from "react";
 import Player from "./Player";
 import "./players.css";
 
+interface PlayerProps {
+    name: string;
+    vagons: number;
+    card1: number;
+    card2: number;
+}
+
 function Players() {
+    let playerArray: PlayerProps[] = [];
+    for (let i = 1; i < 6; i++) {
+        playerArray.push({
+            name: "Player " + i,
+            vagons: Math.floor(Math.random() * 40),
+            card1: Math.floor(Math.random() * 20),
+            card2: Math.floor(Math.random() * 10),
+        });
+    }
+    // eslint-disable-next-line
+    const [players, setPlayers] = useState<PlayerProps[]>(playerArray);
+
     return (
         <div className="game-players">
             <div className="game-players__title">Players</div>
-            <Player />
-            <Player />
-            <Player />
-            <Player />
-            <Player />
+            {[...Array(5)].map((x, i) => (
+                <Player
+                    name={players[i].name}
+                    vagons={players[i].vagons}
+                    card1={players[i].card1}
+                    card2={players[i].card2}
+                />
+            ))}
         </div>
     );
 }

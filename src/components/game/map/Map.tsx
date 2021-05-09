5f8072c2bf3e2797e051191db8c1e9c0002919fc
ngergo0831 <ngergo0831@gmail.com>
2021-05-09 21:00:50 +0200

    i gave up

--- src/components/game/map/Map.tsx
+++ src/components/game/map/Map.tsx
@@ -1,6 +1,8 @@
 import "./map.css";
 import map from "../../../res/map.png";
 import { ticketToRideData } from "../../../res/ticket-to-ride-data";
+import { useState } from "react";
+import { BsCircleFill } from "react-icons/bs";
 
 type Numbers =
     | "1"
@@ -51,61 +53,77 @@ type Numbers =
     | "46"
     | "47";
 
-function FindPosition(oElement: any) {
-    if (typeof oElement.offsetParent != "undefined") {
-        for (
-            var posX = 0, posY = 0;
-            oElement;
-            oElement = oElement.offsetParent
-        ) {
-            posX += oElement.offsetLeft;
-            posY += oElement.offsetTop;
-        }
-        return [posX, posY];
-    } else {
-        return [oElement.x, oElement.y];
-    }
-}
-
-function GetCoordinates(e: any) {
-    let myImg = document.getElementById("map");
-    var PosX = 0;
-    var PosY = 0;
-    var ImgPos;
-    ImgPos = FindPosition(myImg as any);
-    // eslint-disable-next-line
-    if (!e) var e: any = window.event;
-    if (e.pageX || e.pageY) {
-        PosX = e.pageX;
-        PosY = e.pageY;
-    } else if (e.clientX || e.clientY) {
-        PosX =
-            e.clientX +
-            document.body.scrollLeft +
-            document.documentElement.scrollLeft;
-        PosY =
-            e.clientY +
-            document.body.scrollTop +
-            document.documentElement.scrollTop;
-    }
-    PosX = PosX - ImgPos[0];
-    PosY = PosY - ImgPos[1];
-    console.log("PosX: " + PosX);
-    console.log("PosY: " + PosY);
-    // eslint-disable-next-line
-    Object.keys(ticketToRideData.cities).map((key, index) => {
-        if (
-            10.08 * ticketToRideData.cities[key as Numbers].x <= PosX + 30 &&
-            10.08 * ticketToRideData.cities[key as Numbers].x >= PosX - 30 &&
-            6.7358 * ticketToRideData.cities[key as Numbers].y <= PosY + 30 &&
-            6.7358 * ticketToRideData.cities[key as Numbers].y >= PosY - 30
-        ) {
-            console.log(ticketToRideData.cities[key as Numbers].today as any);
-        }
-    });
-}
-
 function Map() {
+    const [isMarked, setMarked] = useState(false);
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    function FindPosition(oElement: any) {
+        if (typeof oElement.offsetParent != "undefined") {
+            for (
+                var posX = 0, posY = 0;
+                oElement;
+                oElement = oElement.offsetParent
+            ) {
+                posX += oElement.offsetLeft;
+                posY += oElement.offsetTop;
+            }
+            return [posX, posY];
+        } else {
+            return [oElement.x, oElement.y];
+        }
+    }
+
+    function GetCoordinates(e: any) {
+        let myImg = document.getElementById("map");
+        var PosX = 0;
+        var PosY = 0;
+        var ImgPos: any;
+        ImgPos = FindPosition(myImg as any);
+        if (!e) e = window.event;
+        if (e.pageX || e.pageY) {
+            PosX = e.pageX;
+            PosY = e.pageY;
+        } else if (e.clientX || e.clientY) {
+            PosX =
+                e.clientX +
+                document.body.scrollLeft +
+                document.documentElement.scrollLeft;
+            PosY =
+                e.clientY +
+                document.body.scrollTop +
+                document.documentElement.scrollTop;
+        }
+        PosX = PosX - ImgPos[0];
+        PosY = PosY - ImgPos[1];
+        console.log("PosX: " + PosX);
+        console.log("PosY: " + PosY);
+        Object.keys(ticketToRideData.cities).forEach((key) => {
+            if (
+                10.352941176 * ticketToRideData.cities[key as Numbers].x <=
+                    PosX + 30 &&
+                10.352941176 * ticketToRideData.cities[key as Numbers].x >=
+                    PosX - 30 &&
+                6.864969325 * ticketToRideData.cities[key as Numbers].y <=
+                    PosY + 30 &&
+                6.864969325 * ticketToRideData.cities[key as Numbers].y >=
+                    PosY - 30
+            ) {
+                setX(
+                    10.352941176 * ticketToRideData.cities[key as Numbers].x +
+                        462
+                );
+                setY(
+                    6.864969325 * ticketToRideData.cities[key as Numbers].y + 4
+                );
+                console.log(
+                    ticketToRideData.cities[key as Numbers].today as any
+                );
+                setMarked(!isMarked);
+            }
+        });
+    }
+
     return (
         <div className="game-map">
             <img
@@ -115,6 +133,17 @@ function Map() {
                 id="map"
                 onMouseDown={GetCoordinates}
             />
+            {isMarked ? (
+                <div
+                    className="point"
+                    style={{ top: y + "px", left: x + "px" }}
+                    onClick={GetCoordinates}
+                >
+                    <BsCircleFill />
+                </div>
+            ) : (
+                ""
+            )}
         </div>
     );
 }
